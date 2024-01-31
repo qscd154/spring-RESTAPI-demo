a4aefa797f92ee4dd9be621552d91b590bee3384
qscd154 <qscd154@gmail.com>
2024-01-31 15:47:29 +0900

    Path 매개변수

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,13 @@ public class HelloWorldController {
     public  HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
 
+    }
+
+    @GetMapping(path =  "/hello-world/path-variable/{name}")
+    public  HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(
+                String.format("Hello Wolrd, %s", name)
+        );
     }
 
 
