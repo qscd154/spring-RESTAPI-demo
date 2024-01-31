4e5bc8a1be3503941a8fd3d2021d35bb947b7a0f
qscd154 <qscd154@gmail.com>
2024-01-31 17:25:16 +0900

    POST 메서드로 사용자 정보 추가하기, HTTP 상태 코드 추가

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,9 +1,10 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,5 +32,16 @@ public class UserResource {
         return uesrDaoService.fidnOne(id);
     }
 
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = uesrDaoService.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
 
 }
