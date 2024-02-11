794a5e28e2cf882d2e912e1dee8563b2b90f3bf8
qscd154 <qscd154@gmail.com>
2024-02-11 18:59:17 +0900

    이름 생일날짜 유효성 검증 기능

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,8 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class User {
@@ -11,7 +14,11 @@ public class User {
     }
 
     private Integer id;
+
+    @Size(min=2, message = "이름은 최소 2글자 이상이어야 합니다." )
     private String name;
+
+    @Past(message = "생일 날짜는 과거여야 합니다.")
     private LocalDate birthDate;
 
 
