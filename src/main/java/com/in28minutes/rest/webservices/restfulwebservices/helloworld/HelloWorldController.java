5231888c4bd1686f4b90787cf05ee2bae51a4662
SUNGDAMFORCE <qscd154@gmail.com>
2024-02-14 19:52:23 +0900

    영어,네덜란드어 응답 제공

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,24 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import com.in28minutes.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
 
     @GetMapping(path =  "/hello-world")
     public  String helloWorld() {
@@ -24,6 +36,15 @@ public class HelloWorldController {
         return new HelloWorldBean(
                 String.format("Hello Wolrd, %s", name)
         );
+    }
+
+    @GetMapping(path =  "/hello-world-internationalized")
+    public  String helloWorldInternationalized() {
+
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default", locale);
+
+        //return "Hello World";
     }
 
 
